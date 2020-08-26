28f1e3c93d72a6e22d70311e90a8facea38f45a3
Son <guswns8431@gmail.com>
2020-08-26 10:34:11 +0900

    emb

---
+++ emb-project/lab1/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "reciprocal.hpp"
+
+int main(int argc,char *argv[])
+{
+	int i;
+	i = atoi(argv[1]);
+	//printf("Hello World\n");
+	//printf("Number of arguments to the main function : %d\n",argc);
+	//	for(i = 0 ; i < argc ; i++)
+	//	{
+			printf("argument numver %d is %s\n",i,reciprocal[i]);
+	//	}
+	return 0;
+}
