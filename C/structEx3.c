ddfe9372f4f2902fb50d3f4e6f8e7362c12b6a16
son <guswns8431@gmail.com>
2020-08-26 09:07:07 +0900

    struct

--- C/structEx3.c
+++ C/structEx3.c
@@ -12,7 +12,7 @@ int main()
     S.jumsu = 92;
     puts("<성적>");
     printf("%d점은 %c학점\n", S.jumsu, S.hakjum);
-    printf("S구조체는 %lu", sizeof(S));
+    printf("S구조체는 %lu바이트입니다", sizeof(S));
 
     return 0;
 }
