8bd498a3937b2bb7ce736719359b3951b0efdd4d
son <guswns8431@gmail.com>
2020-09-03 17:35:15 +0900

    5_1

---
+++ Python/codingdojang/5_1.py
@@ -0,0 +1,29 @@
+# 사칙연산
+print(1+1)
+print(1-1)
+print(2*2)
+print(5/2)
+
+# //연산자
+print(5 // 2)
+
+# %연산자
+
+print(5 % 2)
+
+# **연산자
+
+print(2 ** 10)
+
+#값을 정수로 만들기
+
+print(int(3.3))
+print(int(5/2))
+print(int('10'))
+
+#몫과 나머지를 함께 구하기
+
+print(divmod(5,2))
+
+a,b = divmod(8,3)
+print(a,b)
