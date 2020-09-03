5edfd1b0269cf037328832e865ed43f2eaf86646
son <guswns8431@gmail.com>
2020-09-03 18:13:24 +0900

    7

---
+++ Python/codingdojang/7.py
@@ -0,0 +1,13 @@
+# 변수나 값 여러 개를 콤마로 구분
+print(1, 2, 3)
+print('Hello', 'Python')
+
+# sep로 값 사이에 문자 넣기
+print(1, 2, 3, sep=', ')
+print(1, 2, 3, sep='x')
+print(1, 2, 3, sep='\n')
+
+#end사용하기
+print(1,end='')
+print(2,end='')
+print(3,end='')
