f2a91a8612545347dc7d8c86ad3c588ff5bf44b3
son <guswns8431@gmail.com>
2020-09-03 18:18:38 +0900

    9

---
+++ Python/codingdojang/9.py
@@ -0,0 +1,15 @@
+hello = 'Hello, world!'
+print(hello)
+
+#여러 줄로 된 문자열 사용하기 (multiline string)
+hello = '''Hello, world!!
+안녕하세요.
+Python입니다.'''
+print(hello)
+
+#문자열 안에 작은따옴표나 큰따옴표 포함하기
+s = "Python isn't difficult"
+print(s)
+
+s = 'He said "Python is easy"'
+print(s)
