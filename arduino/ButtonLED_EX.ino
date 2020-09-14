8bda3072d8e50a442d176d13828f40e32366d26c
guswns8431 <45233429+guswns8431@users.noreply.github.com>
2020-09-14 12:47:48 +0900

    Create ButtonLED_EX.ino

---
+++ arduino/ButtonLED_EX.ino
@@ -0,0 +1,33 @@
+int led1 = 4; // led를 몇 번에 연결할건지 설정.4에 연결
+//#define led1 4 이런식으로 초기화하기도함
+#define sw1 2
+
+void setup() {
+  // put your setup code here, to run once:
+  // 한번 수행. 작동을 하기 위한 기본 준비동작 설정
+  pinMode(led1,OUTPUT); //led1이라는 변수가 output이다. 이떄부터 led1이라는 변수를 아두이노가 사용할 수 있음.
+  pinMode(sw1,INPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  // 계속 수행되는 내용
+  boolean reading = digitalRead(sw1); //switch의 값을 읽어와서 reading이라는 변수에 저장
+  if(reading) //switch를 누르면 reading변수가 0이 됨, 안 누른 상태가 1
+  {
+    digitalWrite(led1,HIGH); // off
+    delay(1000);
+  }
+  else
+  {
+    digitalWrite(led1,LOW); // on
+    delay(1000);
+  }
+/*
+  //아래 과정이 무슨 과정인지 이해가 안감
+  digitalWrite(led1,HIGH); // off
+  delay(1000);
+  digitalWrite(led1,LOW); // on
+  delay(1000);
+*/
+}
