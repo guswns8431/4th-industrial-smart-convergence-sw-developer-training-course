b4fda668476e9ab0cc96228ac2b7cbe0cb1c8183
guswns8431 <45233429+guswns8431@users.noreply.github.com>
2020-09-14 15:31:04 +0900

    Update ButtonLED_EX3.ino

--- arduino/ButtonLED_EX3.ino
+++ arduino/ButtonLED_EX3.ino
@@ -1,5 +1,5 @@
 /*
- * Button LED without Delay() 실습하고 
+ * Button LED without Delay() 실습
  */
 const int led1 = 4; // led를 몇 번에 연결할건지 설정.4에 연결
 const int sw1 = 2;
@@ -27,24 +27,42 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   // 계속 수행되는 내용
-  boolean reading = digitalRead(sw1); //switch의 값을 읽어와서 reading이라는 변수에 저장
-  if(reading != lastBtnState)
+  boolean reading = digitalRead(sw1); //상시로 버튼상태 읽기. 스위치가 눌려졌는지 계속 확인
+  
+  if(reading != lastBtnState) // 현재 버튼값이 직전의 버튼상태와 같지 않으면
   {
-    if(!reading)
+    // 버튼 값 표시
+    if(!reading) 
     {
       Serial.print(reading);
       Serial.print("\nlastBtnState = ");
       Serial.println(lastBtnState);
     }
+    
+    //상태변화시 시간 확인, Flag 설정
     Serial.println("first Check");
     lastDebounceTime = millis();
     btnFlag = 1;
     Serial.print("lastTime = ");
     Serial.println(lastDebounceTime);
   }
-
-  if((millis()-lastDebounceTime) > debounceDelay && lastDebounceTime != 0) //delay함수 없이 스위치 제어
+  
+  //상태 변화 이후 시간 감시(버튼의 채터링 대기), 상태 변화 다시 확인
+  if((millis()-lastDebounceTime) > debounceDelay && lastDebounceTime != 0)
   {
+    Serial.print("debounceTime = ");
+    Serial.print(millis()-lastDebounceTime);
+    Serial.print(" ; reading = ");
+    Serial.print(reading);
+    Serial.print(" ; lastBtnState = ");
+    Serial.print(lastBtnState);
+    Serial.print(" ; btnFlag  = ");
+    Serial.print(btnFlag);
+    Serial.print(" ; btnState = ");
+    Serial.println(btnState);
+
+    Serial.print(" ; ledState = ");
+    Serial.println(ledState);
     if(btnFlag)
     {
       btnState = !btnState;
