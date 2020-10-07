6b5a38e528d97eebcecb56ed7c47406fc154d19b
guswns8431 <45233429+guswns8431@users.noreply.github.com>
2020-10-07 14:18:41 +0900

    Create helloworld.c

---
+++ CSharp/helloworld.c
@@ -0,0 +1,14 @@
+using System;
+
+namespace csharp
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Wold!");
+            Console.Write("hihihi");
+            Console.WriteLine("hello world");
+        }
+    }
+}
